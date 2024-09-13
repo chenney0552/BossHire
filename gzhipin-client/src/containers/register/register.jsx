f9c955fd8cd43dc01e98c4d830a46da87b81d285
Gladys Chen Cheng <chen0552@algonquinlive.com>
2024-09-13 00:20:24 -0400

    add the register page

--- gzhipin-client/src/containers/register/register.jsx
+++ gzhipin-client/src/containers/register/register.jsx
@@ -1,13 +1,40 @@
 /*
-* register component
-*/
-import React, {Component} from 'react';
+ * 注册组件
+ */
+import React, { Component } from 'react';
+import { NavBar, WingBlank, List, InputItem, WhiteSpace, Button, Radio } from 'antd-mobile';
+import Logo from '../../components/logo/logo';
+const ListItem = List.Item;
+
 export default class Register extends Component {
     render() {
         return (
             <div>
-                <h1>Register</h1>
+                <NavBar>Boss &nbsp;Hire</NavBar>
+                <Logo />
+                <WingBlank>
+                    <List>
+                        <WhiteSpace />
+                        <InputItem>username</InputItem>
+                        <WhiteSpace />
+                        <InputItem type="password">password</InputItem>
+                        <WhiteSpace />
+                        <InputItem type="password">confirm</InputItem>
+                        <WhiteSpace />
+                        <ListItem>
+                            <span>user type</span>
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <Radio>Candidate</Radio>
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <Radio>Boss</Radio>
+                        </ListItem>
+                        <WhiteSpace />
+                        <Button type="primary">Register</Button>
+                        <WhiteSpace />
+                        <Button>Existing User</Button>
+                    </List>
+                </WingBlank>
             </div>
-        )
+        );
     }
 }
