28967e18eea4782d12a4b0f9a8c93667b91ee42c
Gladys Chen Cheng <chen0552@algonquinlive.com>
2024-09-13 00:38:31 -0400

    finish the register.js

--- gzhipin-client/src/containers/register/register.jsx
+++ gzhipin-client/src/containers/register/register.jsx
@@ -7,6 +7,27 @@ import Logo from '../../components/logo/logo';
 const ListItem = List.Item;
 
 export default class Register extends Component {
+    state = {
+        username: '', // username
+        password: '', // password
+        password2: '', // confirm password
+        userType: 'boss', // user type (candidate or boss)
+    }
+
+    register = () => {
+        console.log(this.state);
+    }
+
+    handleChange = (name, val) => {
+        // 处理输入字段变化的方法
+        // name: 字段名称 (例如: 'username', 'password')
+        // val: 字段的新值
+        // 使用setState更新组件的状态
+        this.setState({
+            [name]: val
+        });
+    }
+
     render() {
         return (
             <div>
@@ -15,23 +36,23 @@ export default class Register extends Component {
                 <WingBlank>
                     <List>
                         <WhiteSpace />
-                        <InputItem>username</InputItem>
+                        <InputItem placeholder="username" onChange={val => {this.handleChange('username', val)}}>username</InputItem>
                         <WhiteSpace />
-                        <InputItem type="password">password</InputItem>
+                        <InputItem placeholder="password" type="password" onChange={val => {this.handleChange('password', val)}}>password</InputItem>
                         <WhiteSpace />
-                        <InputItem type="password">confirm</InputItem>
+                        <InputItem placeholder="confirm password" type="password" onChange={val => {this.handleChange('password2', val)}}>confirm</InputItem>
                         <WhiteSpace />
                         <ListItem>
                             <span>user type</span>
                             &nbsp;&nbsp;&nbsp;&nbsp;
-                            <Radio>Candidate</Radio>
+                            <Radio checked={this.state.userType === 'candidate'} onChange={val => {this.handleChange('userType', 'candidate')}}>Candidate</Radio>
                             &nbsp;&nbsp;&nbsp;&nbsp;
-                            <Radio>Boss</Radio>
+                            <Radio checked={this.state.userType === 'boss'} onChange={val => {this.handleChange('userType', 'boss')}}>Boss</Radio>
                         </ListItem>
                         <WhiteSpace />
-                        <Button type="primary">Register</Button>
+                        <Button type="primary" onClick={this.register}>Register</Button>
                         <WhiteSpace />
-                        <Button>Existing User</Button>
+                        <Button onClick={() => this.props.history.push('/login')}>Existing User</Button>
                     </List>
                 </WingBlank>
             </div>
