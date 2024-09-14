cf038ce2095f73d25e16d801b3027e6a5134004a
Gladys Chen Cheng <chen0552@algonquinlive.com>
2024-09-14 13:57:08 -0400

    finish the header selector component

--- gzhipin-client/src/components/header-selector/header-selector.jsx
+++ gzhipin-client/src/components/header-selector/header-selector.jsx
@@ -3,12 +3,26 @@ user avater selector component
 */
 
 import React, { Component } from 'react';
+import {List, Grid} from 'antd-mobile';
 export default class HeaderSelector extends Component {
+    constructor(props) {
+        super(props);
+        // prepare to render all the avatar
+        this.headerList = [];
+        for (let i = 0; i < 20; i++) {
+            this.headerList.push({
+                text: 'avatar' + (i + 1),
+                icon: require(`./images/头像${i + 1}.png`)
+            });
+        }
+    }
+
     render() {
+        const listHeader = 'Please select avatar';
         return (
-            <div>
-                HeaderSelector
-            </div>
+            <List renderHeader={() => listHeader}>
+                <Grid data={this.headerList} columnNum={5}/>
+            </List>
         );
     }
 }
