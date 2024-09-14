bc17dee6a928b4f85bc11563d591de57e7b68718
Gladys Chen Cheng <chen0552@algonquinlive.com>
2024-09-14 14:01:50 -0400

    finish the dashen information.

--- gzhipin-client/src/containers/dashen-info/dashen-info.jsx
+++ gzhipin-client/src/containers/dashen-info/dashen-info.jsx
@@ -3,12 +3,18 @@
 */
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import { NavBar, InputItem, TextareaItem, Button } from 'antd-mobile';
+import HeaderSelector from '../../components/header-selector/header-selector';
 
 class DashenInfo extends Component {
     render() {
         return (
             <div>
-                DashenInfo
+                <NavBar>Candidate Information Edit</NavBar>
+                <HeaderSelector />
+                <InputItem placeholder='Please input position'>Position</InputItem>
+                <TextareaItem title="Introduce" rows={3} onChange={val => this.handleChange('info', val)} />
+                <Button type='primary'>Save</Button>
             </div>
         );
     }
