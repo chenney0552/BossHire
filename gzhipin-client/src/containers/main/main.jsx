f917f1f4bd96c58f8ba271583f983c1eafa0a57d
Gladys Chen Cheng <chen0552@algonquinlive.com>
2024-09-15 13:27:05 -0400

    redirect to main page when not loggedin

--- gzhipin-client/src/containers/main/main.jsx
+++ gzhipin-client/src/containers/main/main.jsx
@@ -2,12 +2,20 @@
 * main component
 */
 import React, {Component} from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import DashenInfo from '../dashen-info/dashen-info';
 import LaobanInfo from '../laoban-info/laoban-info';
+import {connect} from "react-redux";
 
-export default class Main extends Component {
+class Main extends Component {
     render() {
+        // check if user is logged in, if not, redirect to login page
+        const {user} = this.props
+
+        if (!user._id) {
+            return <Redirect to='/login'/>
+        }
+
         return (
             <div>
                 <Switch>
@@ -18,3 +26,7 @@ export default class Main extends Component {
         )
     }
 }
+
+export default connect(
+    state => ({user: state.user})
+)(Main);
